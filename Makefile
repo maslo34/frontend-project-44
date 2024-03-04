0e28917e88a18c5080e145b467b744497b4800e3
maslo34 <pasha.golyov3021@gmail.com>
2024-03-04 20:10:42 +0300

    add game prime

--- Makefile
+++ Makefile
@@ -8,6 +8,8 @@ brain-calc:
 	node bin/brain-calc.js
 brain-gsd:
 	node bin/brain-gsd.js
+brain-prime:
+	node bin/brain-prime.js
 brain-progression:
 	node bin/brain-progression.js
 publish:
---
+++ src/games/prime.js
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+import { logic } from '../index.js';
+import greetings from './cli.js';
+
+const primeNumber = (char) => {
+    let count = 0 
+    for (let i = 0; i <= char; i +=1) {
+        if (char % i === 0) {
+            count += 1;
+        }
+    }
+    return count === 2? 'yes': 'no';
+}
+
+export default () => {
+    const userName = greetings();
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+    let countAnswer = 0;
+    for (let i = 0; i < 3; i += 1) {
+        const ques =  _.random(1, 50);
+        const result =  primeNumber(ques);
+        const userAnsw = logic(ques, result);
+        if ( userAnsw === true) {
+            countAnswer += 1;
+            console.log('Correct!');
+        } else {
+            break;
+        }
+    }
+    return countAnswer === 3? console.log(`Congratulations, ${userName}!`): console.log(`Let's try again, ${userName}!`);
+    
+};
