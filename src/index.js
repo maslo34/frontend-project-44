8725601d5d4834bf24091c16062bbbc6945da9ab
maslo34 <pasha.golyov3021@gmail.com>
2024-03-05 22:47:38 +0300

    fix ===

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 export default (ques, result) => {
   console.log(`Question: ${ques}`);
   const userAnsw = readlineSync.question('Your answer: ');
-  if (userAnsw == result) {
+  if (Number(userAnsw) === result || userAnsw === result) {
     return true;
   }
   console.log(`'${userAnsw}' is wrong answer ;(. Correct answer was '${result}'.`);
